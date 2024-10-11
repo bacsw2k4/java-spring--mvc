b68f8ee0ada289a03c7286e1db2cfeb661610f66
bacsw2k4 <buihobac2k4@gmail.com>
2024-10-11 21:10:24 +0700

    bac

--- src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -7,9 +7,11 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import vn.hoidanit.laptopshop.domain.User;
 import vn.hoidanit.laptopshop.service.UserService;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 @Controller
 public class UserController {
@@ -31,31 +33,18 @@ public class UserController {
     @RequestMapping("/admin/user")
     public String getUserPage(Model model) {
         String test = this.userService.handlHello();
+        model.addAttribute("newUser", new User());
+        model.addAttribute("buihobac", "from bac dep trai");
 
         return "admin/user/create";
     }
 
     @RequestMapping(value = "/admin/user/create", method = RequestMethod.POST)
-    public String createUserPage(Model model) {
+    public String createUserPage(Model model, @ModelAttribute("newUser") User hoidanit) {
         String test = this.userService.handlHello();
-        System.out.println("run here");
+        System.out.println("run here" + hoidanit);
 
         return "hello";
     }
 
 }
-// @RestController( viet cho mo hinh API)
-// public class UserController {
-
-// private UserService userService;
-
-// public UserController(UserService userService) {
-// this.userService = userService;
-// }
-
-// @GetMapping("")
-// public String getHomePage() {
-// return this.userService.handlHello();
-// }
-
-// }
--- src/main/java/vn/hoidanit/laptopshop/domain/User.java
+++ src/main/java/vn/hoidanit/laptopshop/domain/User.java
@@ -5,70 +5,61 @@ public class User {
     private String email;
     private String password;
     private String fullName;
-    private String phone;
     private String address;
-
-    public User(long id, String email, String password, String fullName, String phone, String address) {
-        this.id = id;
-        this.email = email;
-        this.password = password;
-        this.fullName = fullName;
-        this.phone = phone;
-        this.address = address;
-    }
-
-    public long getId() {
-        return id;
-    }
+    private String phone;
 
     public void setId(long id) {
         this.id = id;
-    }
-
-    public String getEmail() {
-        return email;
     }
 
     public void setEmail(String email) {
         this.email = email;
     }
 
-    public String getPassword() {
-        return password;
-    }
-
     public void setPassword(String password) {
         this.password = password;
-    }
-
-    public String getFullName() {
-        return fullName;
     }
 
     public void setFullName(String fullName) {
         this.fullName = fullName;
     }
 
-    public String getPhone() {
-        return phone;
+    public void setAddress(String address) {
+        this.address = address;
     }
 
     public void setPhone(String phone) {
         this.phone = phone;
     }
 
+    public long getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
     public String getAddress() {
         return address;
     }
 
-    public void setAddress(String address) {
-        this.address = address;
+    public String getPhone() {
+        return phone;
     }
 
     @Override
     public String toString() {
-        return "User [id=" + id + ", email=" + email + ", password=" + password + ", fullName=" + fullName + ", phone="
-                + phone + ", address=" + address + "]";
+        return "User [id=" + id + ", email=" + email + ", password=" + password + ", fullName=" + fullName
+                + ", address=" + address + ", phone=" + phone + "]";
     }
 
 }
